c534c1535f4f24d5135577d6aa8b2ea339c80de5
fedor.turchenko <fedor.turchenko@somin.ai>
2023-07-24 21:10:58 +0300

    add sklearn pipeline compatibility

--- tuning/optuna_tuning.py
+++ tuning/optuna_tuning.py
@@ -1,20 +1,37 @@
 import optuna, numpy as np
+from typing import Literal, Union, Callable
 
 
 class OptunaTuner:
-    def __init__(self, model, error_metric) -> None:
+    def __init__(
+            self,
+            model: Literal['sklearn-model-object'],
+            error_metric: Callable,
+            direction: Union[Literal['maximize'], Literal['minimize']]
+    ) -> None:
         """
         Class constructor
+
+        ## Arguments
+        `model`: Literal['sklearn-model-object']
+            Object of `sklearn` ML model or `sklearn.pipeline.Pipeline`
+
+        `error_metric`: Callable
+            Function to measure model performance
+
+        `direction`: str
+            Either to maximize or minimize performance metric
         """
         self.model = model
         self.error_metric = error_metric
+        self.direction = direction
         optuna.logging.set_verbosity(0)
 
     def fit(self, n_trials: int, X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray, y_test: np.ndarray, *args) -> None:
         """
         Find optimal hyperparameters and fit the model
 
-        ## Parameters
+        ## Arguments
         `n_trials` : int
             Number of optimization iterations
 
@@ -40,7 +57,12 @@ class OptunaTuner:
                 else:
                     params[arg[0]] = trial.suggest_categorical(arg[0], arg[1])
 
-            model = self.model()
+            try:
+                model = self.model()
+            except TypeError:
+                # Account for the case when the object passed is not callable
+                # e.g. `sklearn.pipeline.Pipeline`
+                model = self.model
             model.set_params(**params)
             model.fit(X_train, y_train)
 
@@ -48,10 +70,13 @@ class OptunaTuner:
             error = self.error_metric(y_test, y_pred)
             return error
 
-        study = optuna.create_study(direction='maximize')
+        study = optuna.create_study(direction=self.direction)
         study.optimize(lambda trial: objective(trial, *args), n_trials, n_jobs=7)
 
-        model = self.model()
+        try:
+            model = self.model()
+        except TypeError:
+            model = self.model
         model.set_params(**study.best_params)
         model.fit(X_train, y_train)
 
